520ba003e974fb8b5a0df26a03cb17e8886487b1
alamin-hossen-it <alamin.it24to7@.com>
2025-10-14 18:14:35 +0300

    ref memo zod error handling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react';
+// import { Eye, EyeOff } from 'lucide-react'; // 👈 for icon
+import { IoEye, IoEyeOff } from "react-icons/io5";
+import loginSchema from '../zod/loginSchema';
+
 
 const Login = () => {
-  return (
-    <div className='max-w-6xl mx-auto mt-10 border border-black rounded-md '>
-      <h1 className='mx-auto text-2xl font-bold py-4 px-2 flex justify-center items-center' >Welcome to login page</h1>
-    </div>
-  )
-}
+  const [showPassword, setShowPassword] = useState(false);
+   const [error, setError] = useState({})
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
+ 
 
-export default Login
+
+//   const [email, setEmail] = useState('')
+//   const [password, setPassword] = useState('')
+
+    const changeHandler =(e)=>{
+       const name = e.target.name;
+       const value = e.target.value;
+
+       setFormData({...formData, [name]:value})
+   
+    }
+
+  return (
+    <div className="flex items-center justify-center w-full h-[83vh] bg-[#f0f0f0f0]">
+      
+      <form onSubmit={(e)=>{
+        e.preventDefault()
+        
+        
+     const result = loginSchema.safeParse(formData);
+         if (!result.success) {
+                const errorField = result.error.formErrors.fieldErrors;
+                console.log(errorField)
+                  setError(errorField);
+                  return;
+             }
+            
+             // Start api call
+       
+        //clear form data after submit
+        setFormData({email:'', password:''})
+      }}
+     
+      className="flex flex-col gap-10 bg-white p-10 rounded-lg shadow-2xl w-[40%]">
+        <h1 className='flex justify-center items-center font-bold text-2xl'>Login Please</h1>
+        {/* Email */}
+       <div className='flex flex-col'>
+         <input value={formData.email} name='email' onChange={changeHandler} 
+          className="border border-gray-500 px-3 py-2 rounded-md"
+          type="email" 
+          placeholder="Enter Your Email" 
+        />
+        <span className='text-xs text-red-600'>{error && error.email}</span>
+
+       </div>
+        {/* Password */}
+        <div className="relative">
+          <input value={formData.password} name='password' onChange={changeHandler}
+            className="border border-gray-500 px-3 py-2 rounded-md w-full pr-10"
+            type={showPassword ? 'text' : 'password'} 
+            placeholder="Enter Your Password"
+          />
+             <span className='text-xs text-red-600'>{error && error.password}</span>
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-3 top-2.5 text-gray-600"
+          >
+            {showPassword ? <IoEyeOff size={20} /> : <IoEye size={20} />}
+          </button>
+        </div>
+
+        {/* Login Button */}
+        <button className="border bg-blue-500 px-3 py-2 rounded-md text-white font-bold shadow-2xl hover:bg-blue-600 transition">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/zod/loginSchema.js
@@ -0,0 +1,7 @@
+import {z} from "zod"
+
+const loginSchema = z.object({
+  email: z.string().email("Invalid email format" ),
+  password: z.string().min(6, "Password must be at least 6 characters" ),
+});
+export default loginSchema;
